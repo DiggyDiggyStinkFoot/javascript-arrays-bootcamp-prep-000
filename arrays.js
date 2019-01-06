f988ea2a2beee37793ed227c948eccb045883d90
Wannawuth Damrongchai <wdamrongchai@hotmail.com>
2019-01-06 21:14:11 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -3,7 +3,7 @@ chocolateBars()
 console.log (chocolateBars)
 
 function addElementToBeginningOfArray(a,b){
-addElementToBeginningOfArray()
+addElementToBeginningOfArray(a)
 }
 
 function destructivelyAddElementToBeginningOfArray(a,b){
